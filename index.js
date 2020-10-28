136ab00530bfdbe09de637d3d6096186ea03fb1f
jiang6145 <jiang.notes14@gmail.com>
2020-10-28 15:40:46 +0800

    fs 寫入文件與取得文件,  inly 執行解壓縮

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// 引用 linebot, dotenv, axios 套件 (Node.js 規定 import 全都要放在文件最上面)
+// 引用套件 (Node.js 規定 import 全都要放在文件最上面)
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import axios from 'axios'
+import inly from 'inly'
+import path from 'path'
+import fs from 'fs' // node.js 檔案讀寫的套件
 
 // 讀取 .env 設定檔
 dotenv.config()
@@ -13,27 +16,35 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-// 當使用者傳送訊息時
-bot.on('message', async (event) => {
-  try {
-    const response = await axios.get('https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6')
-    const text = event.message.text
-    let reply = ''
-    for (const data of response.data) {
-      if (data.title === text) {
-        reply = data.showInfo[0].locationName
-        break
-      }
-    }
-
-    reply = (reply.length === 0) ? '找不到資料' : reply
-    event.reply(reply)
-  } catch (error) {
-    event.reply('發生錯誤')
-  }
-})
+const cwd = process.cwd() // 取得 Node 當前執行的工作目錄路徑
+const name = 'TCMSV_alldesc.gz' // 檔案名稱
+const to = cwd
+const from = path.join(cwd, name)
 
 // 監聽 PORT
 bot.listen('/', process.env.PORT, () => {
   console.log('LINE-BOT 已啟動')
 })
+
+// 下載台北市開放資料停車位 API 的 gz檔
+async function getData () {
+  try {
+    // Download File
+    const result = await axios({
+      url: 'https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_alldesc.gz',
+      method: 'get',
+      responseType: 'arraybuffer' // 請求到的檔案類型, 如果沒設定為 arraybuffer 下載後則不會是正確的類型
+    })
+    fs.writeFileSync(from, result.data) // 寫入檔案, 在 Node 環境讀寫檔案要使用 .fs 函式
+    const extract = inly(from, to) // 解壓縮檔案
+
+    // Get Data
+    const file = fs.readFileSync(cwd + '/TCMSV_alldesc') // 取得解壓縮產生的 TCMSV_alldesc 檔案
+    const json = JSON.parse(file)
+    console.log(json)
+  } catch (error) {
+    console.log(error)
+    console.log('找不到檔案')
+  }
+}
+getData()
